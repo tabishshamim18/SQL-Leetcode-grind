7e7ee574cce93f34f3f4d72495e073ad7fe24b37
Tabish Shamim <tabishshamim9@gmail.com>
2026-02-23 12:43:45 +0530

    Time: 539 ms (17.12%) | Memory: 0B (100.00%) - LeetSync

---
+++ 197-rising-temperature/rising-temperature.sql
@@ -0,0 +1,11 @@
+# Write your MySQL query statement below
+/* Write your T-SQL query statement below */
+SELECT
+    w1.id
+FROM
+    weather w1
+JOIN
+    weather w2
+ON
+    w1.temperature > w2.temperature
+    AND DATEDIFF(w1.recordDate, w2.recordDate) = 1
