ea2757951e1bead1c77e885d50dcb96a7ac4ed44
Tabish Shamim <tabishshamim9@gmail.com>
2026-03-05 13:56:18 +0530

    Time: 769 ms (81.07%) | Memory: 0B (100.00%) - LeetSync

---
+++ 1541-top-travellers/top-travellers.sql
@@ -0,0 +1,14 @@
+SELECT
+    name,
+    IFNULL(SUM(distance), 0) AS travelled_distance
+FROM
+    users u
+LEFT JOIN
+    rides r
+ON
+    u.id = r.user_id
+GROUP BY
+    u.id
+ORDER BY
+    travelled_distance DESC,
+    name
