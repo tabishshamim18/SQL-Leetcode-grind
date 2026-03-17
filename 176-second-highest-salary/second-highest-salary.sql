171e37b7e54f598426d899660e028e0ed0c1f790
Tabish Shamim <tabishshamim9@gmail.com>
2026-03-17 23:45:14 +0530

    Time: 287 ms (26.69%) | Memory: 0B (100.00%) - LeetSync

---
+++ 176-second-highest-salary/second-highest-salary.sql
@@ -0,0 +1,12 @@
+# Write your MySQL query statement below
+SELECT
+    IFNULL(
+    (SELECT
+        DISTINCT salary
+    FROM
+        employee
+    ORDER BY salary DESC
+    LIMIT 1
+    OFFSET 1)
+    ,NULL)
+AS SecondHighestSalary 
